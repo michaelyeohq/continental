9dddd73a68458d154d1741ed7397c83acacc2a94
Michael Yeo <mikyeo23@gmail.com>
2021-07-04 03:33:47 +0800

    changed url call

--- src/stores/https/http-api.ts
+++ src/stores/https/http-api.ts
@@ -4,7 +4,10 @@ import axios from 'axios';
 export const httpApi = axios.create({
   baseURL: 'https://digital-challenge-backend.herokuapp.com',
   timeout: 1000,
-  headers: {},
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  },
 });
 
 export default httpApi;
