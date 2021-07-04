c00144138667e6801d945884b0469d5a6c97ee1b
Michael Yeo <mikyeo23@gmail.com>
2021-07-05 05:20:56 +0800

    added cors allow origin

--- src/stores/https/http-api.ts
+++ src/stores/https/http-api.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 export const httpApi = axios.create({
   // baseURL: 'https://digital-challenge-backend.herokuapp.com',
   baseURL: 'https://digital-challenge-backend.herokuapp.com',
-  timeout: 1000,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
